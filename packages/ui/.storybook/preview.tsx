329827fb9ba16f05d529b31b722031a689d45dd5
Michael Reuter <mareuternh@gmail.com>
2026-08-18 07:36:41 -0700

    Add console logging to storybook.

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,10 +1,14 @@
 import type { Preview } from '@storybook/react-vite'
 import { withThemeByDataAttribute } from '@storybook/addon-themes'
 import { INITIAL_VIEWPORTS } from 'storybook/viewport'
-
+import { spyOn } from 'storybook/test'
 import '../index.css'
 
 const preview: Preview = {
+  async beforeEach() {
+    spyOn(console, 'log').mockName('console.log')
+    spyOn(console, 'warn').mockName('console.warn')
+  },
   parameters: {
     controls: {
       matchers: {
