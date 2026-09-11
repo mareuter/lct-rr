9a187d7a959e2ff20961081edde8bc01ee5f972a
Michael Reuter <mareuternh@gmail.com>
2026-09-10 17:24:48 -0700

    Add interaction test to nav bar menu.

--- packages/ui/stories/nav-bar-menu.stories.tsx
+++ packages/ui/stories/nav-bar-menu.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
+import { expect } from 'storybook/test'
+
 import TestSideMenu from './test-side-menu'
 
 const meta: Meta<typeof TestSideMenu> = {
@@ -15,4 +17,11 @@ type Story = StoryObj<typeof TestSideMenu>
 
 export const Default: Story = {
   args: {},
+  play: async ({ canvas, userEvent }) => {
+    await expect(document.getElementById('menu')).not.toHaveClass(
+      'overflow-hidden',
+    )
+    await userEvent.click(canvas.getByRole('button', { name: 'Side Menu' }))
+    await expect(document.getElementById('menu')).toHaveClass('overflow-hidden')
+  },
 }
