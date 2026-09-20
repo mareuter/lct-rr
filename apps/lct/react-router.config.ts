a484c46da080eb7a2e12fd6915cdc4505fffa155
Michael Reuter <mareuternh@gmail.com>
2026-09-19 18:10:50 -0700

    Change app build directory.

--- apps/lct/react-router.config.ts
+++ apps/lct/react-router.config.ts
@@ -2,7 +2,8 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
   // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  buildDirectory: 'dist',
+  // SPA mode
   ssr: false,
   prerender: ['/about'],
 } satisfies Config
