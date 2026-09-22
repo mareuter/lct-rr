3f6964ff0549939408c5fd46ad1891d5eb88da14
Michael Reuter <mareuternh@gmail.com>
2026-09-21 20:54:27 -0700

    Fix date for DateProvider test.

--- packages/ui/lib/tests/date-provider.test.tsx
+++ packages/ui/lib/tests/date-provider.test.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from 'react'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { render } from 'vitest-browser-react'
 import DateProvider from '../helpers/date-provider'
 import DateTester from './date-tester'
@@ -10,8 +10,10 @@ const AllTheProviders = ({ children }: { children: ReactNode }) => {
 
 describe('Test Date Context', () => {
   it('Render the date', async () => {
+    const fixedDate = new Date(2026, 6, 2, 19, 30, 15)
+    vi.setSystemTime(fixedDate)
     const screen = await render(<DateTester />, { wrapper: AllTheProviders })
     const locator = screen.getByRole('paragraph')
-    await expect.poll(() => locator).toHaveTextContent(new Date().toString())
+    await expect.poll(() => locator).toHaveTextContent(fixedDate.toString())
   })
 })
