bf38c44db48d25fb4bf7c56a2c2b45f61944e138
Michael Reuter <mareuternh@gmail.com>
2026-09-24 18:37:39 -0700

    Fix playwright config.

--- apps/lct/playwright.config.ts
+++ apps/lct/playwright.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm start',
-    // url: 'http://localhost:3000',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     // reuseExistingServer: true,
     timeout: 120 * 1000,
